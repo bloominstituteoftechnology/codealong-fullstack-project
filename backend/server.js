44a95a9f1d984abe22374fe7f304e8da1da0af61
Ladrillo <gabriel@cabrejas.net>
2022-04-01 16:39:33 +0200

    wip

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
@@ -9,8 +10,15 @@ const server = express()
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+server.use(express.static(path.join(__dirname, '../dist')))
 
 server.use('/api/auth', validate, authRouter)
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+server.use((req, res, next) => {
+  next({ status: 404, message: 'Not found' })
+})
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(500 || err.status).json({
